4aa98c3f9c35e3aa9c016ea929588a041b58cc23
Tuxx <tuxx@danktank.nl>
2025-07-04 02:06:50 +0200

    Curseforge release

---
+++ package.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ToggleChat CurseForge Package Script
+# This script creates a zip file ready for CurseForge upload
+
+VERSION="1.0.0"
+ADDON_NAME="ToggleChatVisibility"
+PACKAGE_NAME="${ADDON_NAME}-${VERSION}.zip"
+
+echo "Creating CurseForge package for ${ADDON_NAME} v${VERSION}..."
+
+# Create a temporary directory for packaging
+TEMP_DIR=$(mktemp -d)
+PACKAGE_DIR="${TEMP_DIR}/${ADDON_NAME}"
+
+# Create the addon directory structure
+mkdir -p "${PACKAGE_DIR}"
+
+# Copy addon files
+cp ToggleChat.toc "${PACKAGE_DIR}/"
+cp ToggleChat.lua "${PACKAGE_DIR}/"
+cp Bindings.xml "${PACKAGE_DIR}/"
+
+# Create the zip file
+cd "${TEMP_DIR}"
+zip -r "${PACKAGE_NAME}" "${ADDON_NAME}/"
+
+# Move the zip file to the current directory
+mv "${PACKAGE_NAME}" "$(pwd)"
+
+# Clean up
+rm -rf "${TEMP_DIR}"
+
+echo "Package created: ${PACKAGE_NAME}"
+echo "Ready for CurseForge upload!" 
